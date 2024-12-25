25a060d4d42355140e426cd034687dc7f67bd40f
aspauldingcode <aspauldingcode@gmail.com>
2024-12-25 15:39:37 -0800

    don't need a headerfile for DisableTrafficLights.m

--- macwmfx/DisableResizeConstraints.m
+++ macwmfx/DisableResizeConstraints.m
@@ -2,14 +2,25 @@
 
 @interface DisableResizeConstraints : NSObject
 
-- (void)apply;
++ (void)load;
 
 @end
 
 @implementation DisableResizeConstraints
 
-- (void)apply {
-    // Implementation for disabling resize constraints
++ (void)load {
+    NSArray *windows = [NSApp windows];
+    
+    for (NSWindow *window in windows) {
+        // Remove minimum size constraints
+        [window setMinSize:NSMakeSize(0, 0)];
+        
+        // Remove maximum size constraints
+        [window setMaxSize:NSMakeSize(FLT_MAX, FLT_MAX)];
+        
+        // Allow the window to be resized freely
+        [window setStyleMask:[window styleMask] | NSWindowStyleMaskResizable];
+    }
 }
 
 @end 
