d6d271b306fea01712184f17f1bc2f1d2f3ab8f5
aspauldingcode <aspauldingcode@gmail.com>
2024-12-26 14:38:34 -0800

    square corners success

--- macwmfx/CornerRadiusController.m
+++ macwmfx/CornerRadiusController.m
@@ -1,15 +1,28 @@
 #import <Cocoa/Cocoa.h>
+#import <QuartzCore/QuartzCore.h>
+#import "ZKSwizzle.h"
+
+@interface NSWindow (Private)
+- (id)_cornerMask;
+@end
 
 @interface CornerRadiusController : NSObject
-
-- (void)applyWithRadius:(CGFloat)radius;
-
 @end
 
 @implementation CornerRadiusController
 
-- (void)applyWithRadius:(CGFloat)radius {
-    // Implementation for setting custom window corner radius
++ (void)load {
+    Method method = class_getInstanceMethod(NSClassFromString(@"NSWindow"), @selector(_cornerMask));
+    IMP newIMP = imp_implementationWithBlock(^id(__unused id self) {
+        // Create a 1x1 white image for square corners
+        NSImage *squareCornerMask = [[NSImage alloc] initWithSize:NSMakeSize(1, 1)];
+        [squareCornerMask lockFocus];
+        [[NSColor whiteColor] set];
+        NSRectFill(NSMakeRect(0, 0, 1, 1));
+        [squareCornerMask unlockFocus];
+        return squareCornerMask;
+    });
+    method_setImplementation(method, newIMP);
 }
 
-@end 
+@end
