448501e12db198089416d7df663c10c5e281d539
aspauldingcode <aspauldingcode@gmail.com>
2024-12-25 16:07:22 -0800

    add back broken disable titlebars

--- macwmfx/DisableTitleBars.m
+++ macwmfx/DisableTitleBars.m
@@ -1,15 +1,44 @@
 #import <Cocoa/Cocoa.h>
+#import "ZKSwizzle.h"
 
 @interface DisableTitleBars : NSObject
-
-- (void)apply;
-
 @end
 
 @implementation DisableTitleBars
 
-- (void)apply {
-    // Implementation for disabling title bars
++ (void)load {
+    // Initialize the swizzle when the class is loaded
+    [self sharedInstance];
+}
+
++ (instancetype)sharedInstance {
+    static DisableTitleBars *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] init];
+    });
+    return sharedInstance;
+}
+
+@end
+
+ZKSwizzleInterface(BS_NSWindow_TitleBar, NSWindow, NSWindow)
+
+@implementation BS_NSWindow_TitleBar
+
+- (void)makeKeyAndOrderFront:(id)sender {
+    ZKOrig(void, sender);
+    
+    // Disable the title bar
+    [self disableTitleBar];
+}
+
+- (void)disableTitleBar {
+    NSWindow *window = (NSWindow *)self;
+    window.titlebarAppearsTransparent = YES;
+    window.titleVisibility = NSWindowTitleHidden;
+    window.styleMask |= NSWindowStyleMaskFullSizeContentView;
+    window.contentView.wantsLayer = YES; // Ensure contentView is layer-backed
 }
 
 @end 
